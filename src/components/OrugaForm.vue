7882c999c7911de1d5bd112ef09605531b5ba8bb
Andrea Stagi <stagi.andrea@gmail.com>
2020-12-23 23:28:57 +0100

    Add bootstrap

---
+++ src/components/OrugaForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <h1> Tailwind </h1>
+        <form class="o-demo-form">
+            <div class="">
+                <o-field label="Username" label-for="username">
+                    <o-input id="username" type="text" placeholder="Username" />
+                </o-field>
+            </div>
+            <div class="">
+                <o-field label="Password" label-for="password" variant="danger" message="Please choose a password">
+                    <o-input id="password" type="password" placeholder="Password" />
+                </o-field>
+            </div>
+            <div class="">
+                <o-dropdown aria-role="list">
+                    <o-button variant="primary" slot="trigger" slot-scope="{ active }">
+                        <span>Click me!</span>
+                        <o-icon :icon="active ? 'caret-up' : 'caret-down'"></o-icon>
+                    </o-button>
+
+                    <o-dropdown-item aria-role="listitem">Action</o-dropdown-item>
+                    <o-dropdown-item aria-role="listitem">Another action</o-dropdown-item>
+                    <o-dropdown-item aria-role="listitem">Something else</o-dropdown-item>
+                </o-dropdown>
+            </div>
+            <div class="o-demo-form__buttons-wrapper">
+                <div>
+                    <o-button label="Sign In" native-type="button" />
+                    <o-button label="Signup" icon-left="plus" />
+                </div>
+                <div class="">
+                    <o-button label="Forgot password?" outlined />
+                </div>
+            </div>
+        </form>
+    </section>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -8,50 +8,86 @@ import Oruga from '@oruga-ui/oruga'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import OrugaForm from "./components/OrugaForm.vue";
 
-import './assets/tailwindcss.css';
-import './assets/oruga-tailwindcss.css';
+import './assets/common.css';
+import './assets/bootstrap.css';
+// import './assets/tailwindcss.css';
+// import './assets/oruga-tailwindcss.css';
 
 Vue.config.productionTip = false
 
 library.add(fas);
 Vue.component('vue-fontawesome', FontAwesomeIcon);
+Vue.component('oruga-form', OrugaForm);
+
+// Vue.use(Oruga, {
+//     iconPack: 'fas',
+//     iconComponent: 'vue-fontawesome',
+//     statusIcon: false,
+//     button: {
+//         override: true,
+//         rootClass: 'btn',
+//         roundedClass: 'btn-rounded',
+//         outlinedClass: 'btn-outlined',
+//         disabledClass: 'btn-disabled'
+//     },
+//     field: {
+//         override: true,
+//         labelClass: 'field-label',
+//         messageClass: 'text-xs italic',
+//         variantClass: 'field-'
+//     },
+//     input: {
+//         override: true,
+//         inputClass: 'input focus:outline-none focus:shadow-outline',
+//         roundedClass: 'rounded',
+//         variantClass: 'input-'
+//     },
+//     dropdown: {
+//         override: true,
+//         rootClass: 'dropdown',
+//         menuClass: 'dropdown-menu',
+//         itemClass: 'dropdown-item'
+//     },
+//     icon: {
+//         override: true
+//     }
+// })
 
 Vue.use(Oruga, {
-  iconPack: 'fas',
-  iconComponent: 'vue-fontawesome',
-  statusIcon: false,
-  button: {
-    override: true,
-    rootClass: 'btn',
-    roundedClass: 'btn-rounded',
-    outlinedClass: 'btn-outlined',
-    disabledClass: 'btn-disabled'
-  },
-  field: {
-      override: true,
-      labelClass: 'field-label',
-      messageClass: 'text-xs italic',
-      variantClass: 'field-'
-  },
-  input: {
-      override: true,
-      inputClass: 'input focus:outline-none focus:shadow-outline',
-      roundedClass: 'rounded',
-      variantClass: 'input-'
-  },
-  dropdown: {
-      override: true,
-      rootClass: 'dropdown',
-      menuClass: 'dropdown-menu',
-      itemClass: 'dropdown-item'
-  },
-  icon: {
-      override: true
-  }
+    iconPack: 'fas',
+    iconComponent: 'vue-fontawesome',
+    statusIcon: false,
+    button: {
+        override: true,
+        rootClass: 'btn',
+        outlinedClass: 'btn-outline-primary',
+        disabledClass: 'btn-disabled'
+    },
+    field: {
+        override: true,
+        labelClass: 'form-label',
+        messageClass: 'form-text',
+        variantClass: 'field-'
+    },
+    input: {
+        override: true,
+        inputClass: 'form-control',
+        variantClass: 'input-'
+    },
+    dropdown: {
+        override: true,
+        rootClass: 'dropdown',
+        menuClass: 'dropdown-menu show',
+        itemClass: 'dropdown-item'
+    },
+    icon: {
+        override: true
+    }
 })
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
